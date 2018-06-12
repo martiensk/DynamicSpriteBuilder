ca1eabef0a987be74aa21a4e906666babcf5f1dd
Martiens Kropff <martiensk@gmail.com>
2018-06-12 08:57:26 +0100

    initial set up

---
+++ DynamicSpriteBuilder/DynamicSpriteBuilder/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DynamicSpriteBuilder.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; private set; } = "It works!";
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
